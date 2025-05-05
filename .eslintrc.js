053c3f08594cc757134f982191a5b0b5a9a72b80
richardfiller <backup.pst23@gmail.com>
2025-05-05 13:56:19 +0700

    Menambahkan file .eslintrc.js dan menonaktifkan no-useless-escape

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+  ],
+  rules: {
+    'no-useless-escape': 'off',
+    // Tambahkan aturan ESLint lain di sini jika diperlukan
+  },
+};
